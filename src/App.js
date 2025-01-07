df54daa07ffac4139655dedd225431fe606b31b5
Géssica Gabrielle Monteiro Gobbi <34252650+GabrielleGobbi@users.noreply.github.com>
2025-01-07 15:47:03 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
+import React, { useState } from 'react';
 import './App.css';
-import { Header } from './components/Header/Header'; // Importando o Header
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import SearchBar from './components/SearchBar/SearchBar';
 import { WeatherCard } from './components/WeatherCard/WeatherCard';
+import { getWeather } from './components/Api/Api';
 
+function App() {
+  const [weather, setWeather] = useState(null);
+  const [isDarkMode, setIsDarkMode] = useState(false); // Estado para o modo escuro
 
+  const handleSearch = async (city) => {
+    const data = await getWeather(city);
+    setWeather(data);
+  };
 
- function App() {
- 
-    return (
-        <div className="App">
-            <Header />
-              <WeatherCard />
-        </div>
-    );
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode); // Alterna entre claro e escuro
+  };
+
+  return (
+    <div className={`app ${isDarkMode ? 'dark-mode' : ''}`}>
+      <Header toggleDarkMode={toggleDarkMode} isDarkMode={isDarkMode} />
+      <main>
+        <SearchBar onSearch={handleSearch} />
+        {weather && <WeatherCard weather={weather} />}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
- 
-export default App
+export default App;
