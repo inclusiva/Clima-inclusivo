7882bf2621b6710481bf331da0d8782961f83767
Géssica Gabrielle Monteiro Gobbi <34252650+GabrielleGobbi@users.noreply.github.com>
2025-01-09 15:35:38 -0300

    Update api.js

--- src/components/Api/api.js
+++ src/components/Api/api.js
@@ -1,18 +1,41 @@
-const API_KEY = '221ffc853d0fe920bb3bb7b17604e522';
-const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+// src/componentes/Api/Api.js
+
+const OPENWEATHER_KEY = '221ffc853d0fe920bb3bb7b17604e522'; 
+const GEOAPIFY_KEY = 'cf24f5b40b3a446292b21dbdac1b3014';
+
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/forecast';
 
 export async function getWeather(city, units = 'metric') {
-  const url = `${BASE_URL}?q=${city}&units=${units}&appid=${API_KEY}`;
+  const url = `${BASE_WEATHER_URL}?q=${encodeURIComponent(city)}&units=${units}&appid=${OPENWEATHER_KEY}`;
 
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Erro ao buscar dados do clima para ${city}.`);
     }
-
     return await response.json();
   } catch (error) {
     console.error('Erro:', error);
     return null;
+  }
+}
+
+export async function fetchCitySuggestions(query) {
+  const url = `https://api.geoapify.com/v1/geocode/autocomplete?text=${encodeURIComponent(query)}&apiKey=${GEOAPIFY_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.features) {
+      return data.features.map((feature) => ({
+        city: feature.properties.city || feature.properties.town || feature.properties.village || feature.properties.county || '',
+        state: feature.properties.state || '',
+        country: feature.properties.country || ''
+      }));
+    }
+    return [];
+  } catch (error) {
+    console.error('Erro ao buscar sugestões:', error);
+    return [];
   }
 }
