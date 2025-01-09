116141e4f75d98eb3efe63dfd3dd8cf5b4dca19e
Géssica Gabrielle Monteiro Gobbi <34252650+GabrielleGobbi@users.noreply.github.com>
2025-01-09 11:09:02 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-import rodapeImage from "./rodape.png"; // Substitua com o caminho correto da imagem
+// src/componentes/Footer/Footer.jsx
 
-export function Footer() {
+import React from 'react';
+import rodape from './rodape.png'; // Imagem do rodapé
+
+function Footer() {
   return (
     <footer className="footer">
-      <img
-        src={rodapeImage}
-        alt="Logotipo das patrocinadoras da Aceleradora Inclusiva"
-        className="footer-logo"
-      />
-      <p>
-        Contato: <a href="mailto:aceleradora@hotmail.com">aceleradora@hotmail.com</a>
-      </p>
+      <img src={rodape} alt="Imagem do Rodapé" className="footer-image" />
+      <p>Criado pelas alunas da Aceleradora Inclusica 2024 - Todos os direitos reservados © 2025</p>
     </footer>
   );
 }
