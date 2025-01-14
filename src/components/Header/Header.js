b82b32931a759d452ffb70c689b92bde387c386c
Géssica Gabrielle Monteiro Gobbi <34252650+GabrielleGobbi@users.noreply.github.com>
2025-01-14 10:46:37 -0300

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 // src/componentes/Header/Header.jsx
 
 import React from 'react';
-import header from './header.png'; // Imagem do cabeçalho
+import header from './Logo_Inclu_Horz_Cor-06-07.webp'; // Imagem do cabeçalho
 
 function Header({ toggleDarkMode, isDarkMode, toggleLanguage, language }) {
   return (
     <header className="header">
       <img src={header} alt="Imagem do Cabeçalho" className="header-image" />
+      <h1>Previsão do Tempo</h1>
       <div className="header-buttons">
         <button onClick={toggleDarkMode} className="theme-toggle">
           {isDarkMode ? '☀️ Claro' : '🌙 Escuro'}
