8314f47bd2b39f77d5079e14a2c1d6fe1e2bb25e
Géssica Gabrielle Monteiro Gobbi <34252650+GabrielleGobbi@users.noreply.github.com>
2025-01-14 10:43:29 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,25 @@
 // src/componentes/Footer/Footer.jsx
 
 import React from 'react';
-import rodape from './rodape.png'; // Imagem do rodapé
-
+import thoughtworks from './thoughtworks_flamingo_wave.png'; // Imagem do rodapé
+import Tecnopuc from './Tecnopuc.fw.png';
+import PUCRS from './PUCRS.fw.png';
+import Hub_Farol from './Hub_Farol.fw.png';
+import Colibri from './Colibri_Logo_Preferencial_Horizontal_Positivo.png';
 function Footer() {
   return (
     <footer className="footer">
-      <img src={rodape} alt="Imagem do Rodapé" className="footer-image" />
+      <a href="https://www.thoughtworks.com/pt-br/">
+      <img src={thoughtworks} alt="Imagem do thoughtworks" className="footer-image" /></a>
+      <a href="https://tecnopuc.pucrs.br//">
+      <img src={Tecnopuc} alt="Imagem do Tecnopuc" className="footer-image" /></a>
+      <a href="https://portal.pucrs.br//">
+      <img src={PUCRS} alt="Imagem da PUCRS" className="footer-image" /></a>
+      <a href="https://portal.pucrs.br//">
+      <img src={Hub_Farol} alt="Imagem do Hub_Farol" className="footer-image" /></a>
+      <a href="https://tix.life/tecnologia-assistiva/colibri-mouse-de-cabeca/">
+      <img src={Colibri} alt="Imagem do Colibri" className="footer-image" /></a>
+      
       <p>Criado pelas alunas da Aceleradora Inclusica 2024 - Todos os direitos reservados © 2025</p>
     </footer>
   );
