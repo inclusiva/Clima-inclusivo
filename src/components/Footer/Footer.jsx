72670171cef0cedbeb19619bfe2bf8af0374276a
Géssica Gabrielle Monteiro Gobbi <34252650+GabrielleGobbi@users.noreply.github.com>
2025-01-19 18:37:17 -0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,31 @@
-// src/componentes/Footer/Footer.jsx
-
 import React from 'react';
 import thoughtworks from './thoughtworks_flamingo_wave.png'; // Imagem do rodapé
 import Tecnopuc from './Tecnopuc.fw.png';
 import PUCRS from './PUCRS.fw.png';
 import Hub_Farol from './Hub_Farol.fw.png';
 import Colibri from './Colibri_Logo_Preferencial_Horizontal_Positivo.png';
+import globo from './rede-globo-logo-4.png';
+
 
 function Footer({ language }) {
   return (
     <footer className="footer">
-      <a href="https://www.thoughtworks.com/pt-br/">
-        <img src={thoughtworks} alt="Imagem do thoughtworks" className="footer-image" />
+       <a href="https://www.globo.com/" target="_blank" rel="noopener noreferrer">
+        <img src={globo} alt="Imagem da globo" className="footer-image" />
       </a>
-      <a href="https://tecnopuc.pucrs.br/">
+      <a href="https://www.thoughtworks.com/pt-br/" target="_blank" rel="noopener noreferrer">
+        <img src={thoughtworks} alt="Imagem da thoughtworks" className="footer-image" />
+      </a>
+      <a href="https://tecnopuc.pucrs.br/" target="_blank" rel="noopener noreferrer">
         <img src={Tecnopuc} alt="Imagem do Tecnopuc" className="footer-image" />
       </a>
-      <a href="https://portal.pucrs.br/">
+      <a href="https://portal.pucrs.br/" target="_blank" rel="noopener noreferrer">
         <img src={PUCRS} alt="Imagem da PUCRS" className="footer-image" />
       </a>
-      <a href="https://portal.pucrs.br/">
-        <img src={Hub_Farol} alt="Imagem do Hub Farol" className="footer-image" />
+      <a href="https://tecnopuc.pucrs.br/ecossistema-tecnopuc/hubs/" target="_blank" rel="noopener noreferrer">
+        <img src={Hub_Farol} alt="Imagem do Farol Social Hub " className="footer-image" />
       </a>
-      <a href="https://tix.life/tecnologia-assistiva/colibri-mouse-de-cabeca/">
+      <a href="https://tix.life/tecnologia-assistiva/colibri-mouse-de-cabeca/" target="_blank" rel="noopener noreferrer">
         <img src={Colibri} alt="Imagem do Colibri" className="footer-image" />
       </a>
       <p>
